393d08cc9e8577eac9e8948bf117c23f30ead3db
im_nt <imantahir707@gmail.com>
2025-02-21 09:01:08 +0500

    Added Firstlab and Car classes with basic functionality.

---
+++ Lab1/Firstlab.java
@@ -0,0 +1,12 @@
+package Lab1;
+public class Firstlab{
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = sum(a, b);
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
