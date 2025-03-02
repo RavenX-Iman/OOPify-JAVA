3898b4ce40ffd1257bc8fb88673458f5b5897b0a
im_nt <imantahir707@gmail.com>
2025-03-02 15:21:38 +0500

    Add Book class and enhance OOPS example with multiple Pen objects.

---
+++ theory/Book.java
@@ -0,0 +1,8 @@
+package theory;
+
+class Book {
+    private String author;
+    private String[] chapter_names= new String[100];
+    //constructors object as a returntype basically to lab1- lab4.
+    
+}
--- theory/OOPS.java
+++ theory/OOPS.java
@@ -26,11 +26,22 @@ class Pen{
 public class OOPS{
     public static void main(String[] args) {
         Pen pen1 = new Pen(); //creating an object of class pen
+        //setting values of objects(properties of pen 1)
         pen1.color="Blue";
         pen1.type="Gel-pen";
         pen1.price=10;
+
         pen1.write();
         pen1.printcolor();
+
+        Pen pen2 = new Pen(); //creating a new object of class pen
+        //setting values of objects(properties of pen 2)
+        pen2.color="Red"; 
+        pen2.type="Ballpoint";
+        pen2.price=5;
+
+        pen2.write();
+        pen2.printcolor();
 
 
     }
