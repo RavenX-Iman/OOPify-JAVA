356d498bb170930030d1f3d44a50a711fa6d5619
im_nt <imantahir707@gmail.com>
2025-03-05 23:23:47 +0500

    Added Circle class and average calculation in abc class for geometric computations.

---
+++ Lab1/abc.java
@@ -0,0 +1,7 @@
+public class abc {
+    public double average (){
+        int sum = Array.stream(result_Array);
+        sum();
+        return sum/5.0;
+    }
+}
---
+++ theory/Day1.java
@@ -0,0 +1,45 @@
+package theory;
+    class Circle{
+        double radius;
+    
+        //Constructors
+        Circle(){
+            radius = 1;
+        }
+    
+        Circle(double newradius){
+            radius= newradius;
+        }
+    
+        //setfunction
+        void setRadius(double newradius) {
+            radius = newradius;
+        }
+        //get functions
+        double getArea(){
+            return (radius * radius * 3.14);
+        }
+    
+        double getPerimeter(){
+            return (radius * 2 * 3.14);
+        }
+    }
+    
+    public class Day1 {
+        public static void main(String[]args){
+            // object creation
+            Circle c1 = new Circle();
+            System.out.println("Area of the Circle of radius "+ c1.radius + "is" + c1.getArea());
+    
+            Circle c2 = new Circle();
+            System.out.println("Area of the Circle of radius "+ c2.radius + "is" + c2.getArea());
+    
+            Circle c3 = new Circle();
+            System.out.println("Area of the Circle of radius "+ c3.radius +"is" + c1.getPerimeter());
+    
+            c2.setRadius(100);
+            System.out.println("Area of the Circle of radius " + c2.radius + "is" + c2.getArea());
+    
+        }
+    }
+
