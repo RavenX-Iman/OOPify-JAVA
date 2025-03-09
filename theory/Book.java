d6ecc0c0f928d20c3b88450e9c8def55842f7f1c
im_nt <imantahir707@gmail.com>
2025-03-09 17:00:15 +0500

    Add Student class with methods for data input, grade calculation, and display; create main class for testing.

---
+++ theory/Book.java
@@ -0,0 +1,15 @@
+package theory;
+import java.util.Scanner;
+public class Book {
+    //attributes of book
+    private String name;
+    private String author;
+    private int price;
+
+    //default constructor with no arguments
+    Book(){
+        name=null;
+        author=null;
+    }
+    
+}
