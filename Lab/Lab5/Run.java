b5acf3f779ea4519fc9271f72dc5755a5a4c94e2
im_nt <imantahir707@gmail.com>
2025-03-13 10:42:07 +0500

    Add Person, Student, and Manager classes with methods for data handling and display; implement main methods for testing functionality.

---
+++ Lab/Lab5/Run.java
@@ -0,0 +1,94 @@
+package Lab5;
+class Person {
+    //parent class
+    private String name;
+    private String address;
+    private String ph_No;
+    private int id;
+
+    Person(){
+        name=null;
+        address=null;
+        ph_No=null;
+        id=0;
+    }
+    Person(String n, String a,String p,int i){
+        name=n;
+        address=a;
+        ph_No=p;
+        id=i;
+    }
+    public void setName(String n){
+        name=n;
+    }
+    public String getName (String n){
+        return name;
+    }
+    public void  setAddress(String a){
+        address=a;
+    }
+    public String getAdress(String a){
+        return name;
+    }
+    public void setPh_No(String p){
+        ph_No=p;
+    }
+    public String getPh_No(String p){
+        return name;
+    }
+    public void setId(int i) {
+        id = i;
+    }
+    public int getId() {
+        return id;
+    }
+    // methods
+    public void Display(){
+        System.out.println("Name: "+name);
+        System.out.println("Address: "+address);
+        System.out.println("Phone-no: "+ph_No);
+        System.out.println("ID: "+id);
+
+    }
+}
+class Student extends Person{
+    //child class
+    private String semester;
+    private int roll_no;
+
+    Student(){
+        semester=null;
+        roll_no=0;
+    }
+    Student(String n, String a,String p,int i,String s,int r){
+        super(n, a, p, i);
+        semester=s;
+        roll_no=r;
+    }
+    public void setPh_No(String s){
+        semester=s;
+    }
+    public String getPh_No(String s){
+        return semester;
+    }
+    public void setId(int r) {
+        roll_no = r;
+    }
+    public int getId() {
+        return roll_no;
+    }
+
+    public void Display(){
+        super.Display();
+        System.out.println("Semester: "+semester);
+        System.out.println("Roll-No: "+roll_no);
+    }
+}
+public class Run{
+    public static void main(String[] args) {
+        // Person p1 = new Person();
+        //we will not make object of parent class and not call it because we can call them from child class
+        Student s1 = new Student("Aliza","xyz","12345",456,"3rd",1234);
+        s1.Display();
+        }
+}
---
+++ Lab/Lab5/Runner.java
@@ -0,0 +1,89 @@
+package Lab5;
+class studentRecord { 
+    //child class
+ 
+    private String degree; 
+ 
+    studentRecord() { 
+    } 
+
+    public void setDegree(String deg) { 
+        degree = deg; 
+ 
+    } 
+ 
+    public String getDegree() { 
+        return degree; 
+    } 
+ 
+} 
+ 
+class employeeRecord { 
+    //child class
+ 
+    private int emp_id; 
+    private double salary; 
+ 
+    public employeeRecord() { 
+    } 
+ 
+    public void setEmp_id(int id) { 
+        emp_id = id; 
+    } 
+ 
+    public int getEmp_id() { 
+        return emp_id; 
+    } 
+ 
+    public void setSalary(int sal) { 
+        salary = sal; 
+    }
+    public double getSalary() { 
+        return salary; 
+    } 
+ 
+} 
+ 
+class Manager { 
+ 
+    private String title; 
+    private double dues; 
+    private employeeRecord emp; 
+    private studentRecord stu; 
+ 
+    public Manager(String t, double d, employeeRecord e, studentRecord s) { 
+        title = t; 
+        dues = d; 
+        emp = e; 
+        stu = s; 
+    } 
+ 
+    public void display() { 
+ 
+        System.out.println("Title is : " + title); 
+        System.out.println("Dues are : " + dues); 
+ 
+        System.out.println("Emplyoyee record is as under:"); 
+        System.out.println("EmployeeId is : " + emp.getEmp_id()); 
+        System.out.println("EmployeeId is : " + emp.getSalary()); 
+ 
+        System.out.println("Student record is as under: "); 
+ 
+        System.out.println("Degree is : " + stu.getDegree()); 
+    } 
+ 
+} 
+ 
+public class Runner { 
+ 
+    public static void main(String args[]) { 
+        studentRecord s = new studentRecord(); 
+        s.setDegree("MBA"); 
+        employeeRecord e = new employeeRecord(); 
+        e.setEmp_id(1); 
+        e.setSalary(25000); 
+        Manager m1 = new 
+Manager("financeManager", 5000, e, s); 
+        m1.display(); 
+    } 
+} 
