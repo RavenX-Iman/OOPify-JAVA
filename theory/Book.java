2e7259a51074ce5b9efc4487b96c082fdf492e1c
im_nt <imantahir707@gmail.com>
2025-03-19 22:53:34 +0500

    Add Book class with methods for input and display; implement main method for testing functionality.

--- theory/Book.java
+++ theory/Book.java
@@ -10,6 +10,27 @@ public class Book {
     Book(){
         name=null;
         author=null;
+        price=0;
+    }
+    void input(){//method to input values
+        Scanner sc = new Scanner(System.in); //
+        System.out.println("Enter book name: ");
+        name=sc.nextLine();
+        System.out.println("Enter book author: ");
+        author=sc.nextLine();
+        System.out.println("Enter book price: ");
+        price=sc.nextInt();
+        sc.close();
+    }
+    void display(){ //method to display values
+        System.out.println("Book name: "+ name);
+        System.out.println("Book author: "+ author);
+        System.out.println("Book price: "+ price);
     }
     
+    public static void main(String[] args) {
+        Book oop = new Book(); //object creation
+        oop.input(); //input method called
+        oop.display();//display method called
+    }
 }
