f850b3306b99a4a81a466b8ee2d95d75a3dcc0a5
im_nt <imantahir707@gmail.com>
2025-05-15 11:37:01 +0500

    added circle class to calculate circumference

---
+++ Lab/Lab2/Task1.java
@@ -0,0 +1,21 @@
+package Lab2;
+class Circle{
+    public double radius;
+    Circle(){
+        radius=0.0;
+    }
+    Circle(double r){
+        radius=r;
+    }
+    public void circumference(){
+        double circ=2*3.146 *radius;
+        System.out.println(circ);
+    }
+}
+public class Task1 {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(5);
+        c1.circumference();
+    }
+    
+}
